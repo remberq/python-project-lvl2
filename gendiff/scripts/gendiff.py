b39d6df211ca67e48d110071f4a3d97bf59483a2
AndreySol <remberq@Zen.localdomain>
2021-12-24 22:25:19 +0300

    second stage commit

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,6 +4,7 @@ import argparse
 
 def main():
     parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('-f', '--format', help='ser format of output')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     args = parser.parse_args()
